2115f2c100744ca227ed9b4c223c30f5e6f51c3b
Roberto Cerna <164534193+robertoCerna1055@users.noreply.github.com>
2025-02-07 16:48:28 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,21 @@
+// Funcion que no devuelva valor 
+// Funcion saludar
+
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('Roberto Cerna');
+}
+
+void main(){
+  // Roberto Cerna Mat: 22308051281055
+  // Llamar a la funcion saludar
+  print('Roberto Cerna Mat: 22308051281055');
+  print('Llamando a la funcion saludar');
+  saludar();
+  print('Llamando a la funcion escribeNombre');
+  escribeNombre();
+
+}
