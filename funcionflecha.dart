433c3335f6ae438510e9e6f1531ecad75f314f05
Roberto Cerna <164534193+robertoCerna1055@users.noreply.github.com>
2025-02-10 18:32:24 +0000

    publique funcion flecha

---
+++ funcionflecha.dart
@@ -0,0 +1,27 @@
+int multiplicacion(int a, int b){
+  return a * b;
+}
+
+int flechamultiplicacion(int a, int b) => a * b;
+
+double division(int a, int b){
+  return a / b;
+}
+
+double flechadivision(int a, int b) => a / b;
+
+
+
+// returnType functionName(parameters...) => expression;
+//Funcio main
+void main(){
+  print('Roberto Cerna Mat: 22308051281055');
+  print('Llamando a la funcion multiplicación');
+  print(multiplicacion(10, 5));
+  print('Llamando a la funcion flechamultiplicacion');
+  print(flechamultiplicacion(3, 5));
+    print('Llamando a la funcion division');
+  print(division(10, 3));
+  print('Llamando a la funcion flechadivision');
+  print(flechadivision(10, 3));
+}
--- recursos/funcionsuma.md
+++ recursos/funcionsuma.md
@@ -1,2 +1,6 @@
 Ejemplo de salida
 ![alt text](image.png)
+
+Ejemplo de salida funcion
+normal + funcion flecha
+![alt text](image-1.png)
